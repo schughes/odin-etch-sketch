466bce1e9a6a1700b9eea78c3947c68c2817350d
Samantha Hughes <samantha.hughes@quantumworkplace.com>
2023-10-10 10:31:43 -0500

    update grid

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function createGrid() {
-  let n = 4;
+  let n = 8;
   for (let i = 1; i <= n; i++) {
     let row = document.createElement("div");
     row.id = `r${i}`;
     row.classList.add("row");
     for (let j = 1; j <= n; j++) {
+      let box = document.createElement("div");
+      box.classList.add("box");
       let div = document.createElement("div");
       div.classList.add("square");
-      row.appendChild(div);
+      box.appendChild(div);
+      row.appendChild(box);
     }
     document.getElementById("grid").appendChild(row);
   }
